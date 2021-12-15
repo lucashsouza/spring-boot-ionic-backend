b3e4b97a1f0f1c01587285be741ddf8156ea19ce
lucashsouza <lhenriquesouza00@gmail.com>
2021-12-14 23:43:38 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/br/com/lucashsouza/cursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package br.com.lucashsouza.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.lucashsouza.cursomc.domain.Pedido;
+import br.com.lucashsouza.cursomc.repositories.PedidoRepository;
+import br.com.lucashsouza.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repository;
+	
+	public Pedido buscar(Integer id) {
+		
+		Optional<Pedido> categoria = repository.findById(id);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! " +
+			      "ID: " + id + " " +
+			      "Tipo: " + Pedido.class.getName()));
+	}
+	
+}
