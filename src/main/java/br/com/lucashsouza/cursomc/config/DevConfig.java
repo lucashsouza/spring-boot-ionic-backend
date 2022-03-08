1eb4b7d8a4eaba4ce02928b918125a7ecf2cfb5e
lucashsouza <lhenriquesouza00@gmail.com>
2022-03-07 23:30:44 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/br/com/lucashsouza/cursomc/config/DevConfig.java
+++ src/main/java/br/com/lucashsouza/cursomc/config/DevConfig.java
@@ -1,6 +1,8 @@
 package br.com.lucashsouza.cursomc.config;
 
 import br.com.lucashsouza.cursomc.services.DBService;
+import br.com.lucashsouza.cursomc.services.EmailService;
+import br.com.lucashsouza.cursomc.services.SmtpEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -28,6 +30,11 @@ public class DevConfig {
 
         dbService.instantiateTestDatabase();
         return true;
+    }
+
+    @Bean
+    public EmailService emailService() {
+        return new SmtpEmailService();
     }
 
 }
